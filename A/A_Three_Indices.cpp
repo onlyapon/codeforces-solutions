be47067bb86394d34efe7c683391eb24d5a6cf38
onlyapon <ajmain.apon123@gmail.com>
2025-05-23 23:52:31 +0600

    May 23

---
+++ A/A_Three_Indices.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;cin>>n;
+        vector<int> v(n);
+        cin>>v[0];
+        int first=v[0];
+        bool first_f=false;
+        int x=1,y,z;
+        vector<pair<int,int>> p;
+        for (int i = 1; i < n; i++)
+        {
+            cin>>v[i];
+            if (v[i-1]<v[i])
+            {
+                p.push_back({i,i+1});
+            }
+             
+        }
+        for(auto i:p){
+            for (int j = i.second; j < n; j++)
+            {
+                if (v[i.second-1]>v[j])
+                {
+                    cout<<"YES\n"<<i.first<<" "<<i.second<<" "<<j<<"\n";
+                    return;
+                }
+                
+            }
+            
+        }
+        cout<<"NO\n";
+}
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        solve();
+        
+        
+    }
+    
+}
---
+++ A/A_Two_Permutations.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        int n,a,b;
+        cin>>n>>a>>b;
+        if (n==1 and b==1 and a==1)
+        {
+            cout<<"YES\n";
+            continue;
+        }
+        
+        if (a+b>n or a+b+2>n)
+        {
+            cout<<"NO\n";
+        }else{
+            cout<<"YES\n";
+        }
+        
+    }
+    
+}
